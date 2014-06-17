2098fd4552442560da6f6e6876570706b4cca190
descent <descent0@gmail.com>
2014-06-17 23:15:08 +0800

    new file:   1.2.4/expo_0.scm 	new file:   1.2.4/expo_1.scm 	new file:   1.2.4/expo_2.scm 	new file:   1.2.4/gcd.scm

---
+++ 1.2.4/expo_2.scm
@@ -0,0 +1,13 @@
+(define (fast-expt b n)
+  (cond ((= n 0) 1)
+        ((even? n) (square (fast-expt b (/ n 2))))
+        (else (* b (fast-expt b (- n 1))))))
+
+(define (even? n)
+  (= (remainder n 2) 0))
+
+(fast-expt 2 4)
+
+(even? 2)
+
+
