0d02c07118f9e6db8f36cf707424d5269ec43a07
descent <descent0@gmail.com>
2014-08-11 11:11:18 +0800

    new file:   1.2.1/factorial.scm

---
+++ 1.2.1/factorial.scm
@@ -0,0 +1,17 @@
+; recursive
+(define (factorial n)
+  (if (= n 1)
+    1
+    (* n (factorial (- n 1)))))
+(factorial 5)
+
+; iteration
+(define (factorial n)
+  (fact-iter 1 1 n))
+  
+(define (fact-iter product counter max-count)
+  (if (> counter max-count)
+    product
+    (fact-iter (* counter product)
+               (+ counter 1)
+               max-count)))
