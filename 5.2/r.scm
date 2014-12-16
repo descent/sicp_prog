8f34f4bc417b05d99d81bbc3a42beec2aaa6a712
descent <descent0@gmail.com>
2014-12-16 20:17:00 +0800

    add test code

--- 5.2/r.scm
+++ 5.2/r.scm
@@ -1,20 +1,57 @@
-(define gcd-machine 
-  (make-machine 
-    '(a b t)
-    (list (list 'rem remainder) (list '= =))
-    '(test-b  
-  (test (op = ) (reg b ) (const 0))
-  (branch (label gcd-done))
-  (assign t (op rem) (reg a ) (reg b))
-  (assign a (reg b))
-  (assign b (reg t))
-  (goto (label test-b))
- gcd-done)))
+;(define gcd-machine 
+;  (make-machine 
+;    '(a b t)
+;    (list (list 'rem remainder) (list '= =))
+;    '(test-b  
+;  (test (op = ) (reg b ) (const 0))
+;  (branch (label gcd-done))
+;  (assign t (op rem) (reg a ) (reg b))
+;  (assign a (reg b))
+;  (assign b (reg t))
+;  (goto (label test-b))
+; gcd-done)))
 
 
-(set-register-contents! gcd-machine 'a 206)
-(set-register-contents! gcd-machine 'b 40)
-(start gcd-machine)
-(get-register-contents gcd-machine 'a)
+
+(define (extract-labels text)
+  (if (null? text)
+      (cons '() '())
+      (let ((result (extract-labels (cdr text))))
+        (let ((insts (car result)) (labels (cdr result)))
+          (let ((next-inst (car text)))
+            (if (symbol? next-inst)
+                (cons insts
+                      (cons (make-label-entry next-inst insts) labels))
+                (cons (cons (make-instruction next-inst) insts)
+                      labels))))) ))
+
+
+;(define (extract-labels text)
+;        (display "\n\!!\n")
+;        (display text)
+;        (display "\n\!!!!\n")
+;  (if (null? text)
+;      (cons '() '())
+;      (let ((result (extract-labels (cdr text))))
+;        (display "\n\!!\n")
+;        (display result)
+;        (display "\n\!!!!\n")
+;        result
+;        )))
+
+(extract-labels '(test-b (test (op = ) (reg b ) (const 0))))
+
+;(define gcd-machine 
+;  (make-machine 
+;    '(a b t)
+;    (list (list 'rem remainder) (list '= =))
+;    '(test-b  
+;  (test (op = ) (reg b ) (const 0))
+; )))
+
+;(set-register-contents! gcd-machine 'a 206)
+;(set-register-contents! gcd-machine 'b 40)
+;(start gcd-machine)
+;(get-register-contents gcd-machine 'a)
 
 
